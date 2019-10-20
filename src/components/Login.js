43642eb82f25d233c267749a7152ae33ffc1f89b
hajikomi <hajime0829@gmail.com>
2019-10-21 06:21:22 +0900

    ログイン画面更新

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,93 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
-const useStyles = makeStyles({
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
+
+const styles = {
+    constyle:{
+        margin:0,
+        padding:0,
+        fontFamily: "メイリオ",
+        boxSizing: "border-box",
+        textDecoration: "none",
+        listStyleType: "none",
+        textAlign:"center",
     },
-  });
+    paper:{
+        margin:"0 auto",
+        padding:"3% 5%",
+        width:"45%",
+    
+        backgroundColor:"#EEEEEE",
+        textAlign:"center",
+    },
+    input:{
+        border:1,
+        borderRadius: 3,
+    },
+    standard:{
+        textAlign:"left",
+        width:"70%",
+        margin:"0 auto",
+    }
+}
 
-class Login extends React.Component{
+class Login extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            click:false
+        }
+    }
+    changeClick(){
+        this.setState({click:!this.state.click});
+    }
+    passChange(){
+        if (this.state.click) {
+            return(
+                <TextField style={{backgroundColor:"#EEFFFF"}} type="text" margin="dense" 
+                            variant="outlined" fullWidth="true" label="パスワード" required="true"/>
+            );
+        }else{
+            return(
+                <TextField style={{backgroundColor:"#EEFFFF"}} type="password" margin="dense" 
+                                variant="outlined" fullWidth="true" label="パスワード" required="true"/>
+            );
+        }
+    }
+
+
     render(){
-        const classes = useStyles();
+        const { classes } = this.props;
         return(
             <div>
-                <Container maxWidth="md">
-                    <Typography align="center" variant="h3">
+                <Container className={classes.constyle}>
+                    <Typography align="center" variant="h3" style={{margin:"30px"}}>
                         Roof Tops
-                        <Button　className={classes.root}>テスト</Button>
                     </Typography>
-                    <Button　color="inherit" size="large" variant="contained">
-                        ログイン
-                    </Button>
+                    <Paper className={classes.paper}>
+                        <div className={classes.standard}>
+                            <TextField style={{backgroundColor:"#EEFFFF"}} margin="dense" 
+                            variant="outlined" fullWidth="true" label="アカウント" required="true"/>
+                            {this.passChange()}                            
+                            <FormControlLabel control={<Checkbox color="default" checked={this.state.click} onClick={() => this.changeClick()}/>} label="パスワードを表示する" />         
+                            <Button fullWidth="true" style={{marginTop:"10%",backgroundColor:"#00CC00",color:"white"}}>ログイン</Button>
+                        </div>
+                    </Paper>
                 </Container>
             </div>
         );
     }
 }
 
-export default Login
+//Login.propTypes = {
+    //classes: PropTypes.object,
+//};
+
+export default withStyles(styles)(Login);
---
+++ src/components/commonStyle.js
@@ -0,0 +1,12 @@
+
+const constyle = {
+    margin:0,
+    padding:0,
+    fontFamily: "メイリオ",
+    boxSizing: "border-box",
+    textDecoration: "none",
+    listStyleType: "none",
+    textAlign:"center",
+}
+
+export default constyle;
